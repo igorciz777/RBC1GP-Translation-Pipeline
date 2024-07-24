b7a3a9d29826f50fb591a3c1d285f50d0df6be5e
igor c <75314224+igorciz777@users.noreply.github.com>
2024-07-24 21:53:06 +0200

    Add files via upload

---
+++ c1gp_atlas_cart/atlas.bat
@@ -0,0 +1,7 @@
+.\ext\Atlas.exe -d stdout .\bin\00000224.bin .\out\224.txt
+.\ext\Atlas.exe -d stdout .\bin\00000227.bin .\out\227.txt
+.\ext\Atlas.exe -d stdout .\bin\00000228.bin .\out\228.txt
+.\ext\Atlas.exe -d stdout .\bin\00000229.bin .\out\229.txt
+.\ext\Atlas.exe -d stdout .\bin\00000230.bin .\out\230.txt
+.\ext\Atlas.exe -d stdout .\bin\00000231.bin .\out\231.txt
+.\ext\Atlas.exe -d stdout .\bin\00000737.bin .\out\737.txt
---
+++ c1gp_atlas_cart/cartographer.bat
@@ -0,0 +1,7 @@
+.\ext\Cartographer.exe .\bin\00000224.bin .\cartographer_scripts\c1gp_224.txt .\out\224 -s
+.\ext\Cartographer.exe .\bin\00000227.bin .\cartographer_scripts\c1gp_227.txt .\out\227 -s
+.\ext\Cartographer.exe .\bin\00000228.bin .\cartographer_scripts\c1gp_228.txt .\out\228 -s
+.\ext\Cartographer.exe .\bin\00000229.bin .\cartographer_scripts\c1gp_229.txt .\out\229 -s
+.\ext\Cartographer.exe .\bin\00000230.bin .\cartographer_scripts\c1gp_230.txt .\out\230 -s
+.\ext\Cartographer.exe .\bin\00000231.bin .\cartographer_scripts\c1gp_231.txt .\out\231 -s
+.\ext\Cartographer.exe .\bin\00000737.bin .\cartographer_scripts\c1gp_737.txt .\out\737 -s
